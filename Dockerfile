4983e79e11ba01c8859d5aaebad1497382d8474b
JUNZ1 <gigabayt@gmail.com>
2024-07-23 19:30:51 +0200

    Curl is added to repository

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ RUN apt-get install git -y
 RUN apt-get install cmake -y
 RUN apt-get install build-essential -y
 RUN apt-get install gdb -y
+RUN apt-get install libssh2-1-dev -y
+RUN apt-get install libpsl-dev -y
 WORKDIR /home/workspace/
 COPY . .
--- mainCPP/main.cpp
+++ mainCPP/main.cpp
@@ -1,36 +1,9 @@
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <cstring>
 #include <iostream>
-#include <vector>
-#include <cstdint>
-#include <numeric>
-#include <fstream>
-#include <thread>
-#include <future>
+
+#include "curl/curl.h"
 using namespace std;
 
-bool func(string write_this, unsigned int this_number){
-    for(unsigned int counter = 0; counter < this_number; ++counter){
-    cout<<write_this<<endl;
-    this_thread::sleep_for(chrono::milliseconds(100));
-    }
-    return true;
-}
-
 int main() {
-    unsigned int thread_number = 5;
-    std::vector<future<bool>> future_list;
-    for(unsigned int counter = 0; counter < thread_number;++counter){
-        future_list.push_back(async(launch::async,func, "thread"+std::to_string(counter), 100));
-    }
-
-    unsigned int return_counter;
-
-    for(auto& each_future : future_list){
-        cout<<each_future.get()<<endl;
-    }
+    std::cout<<"compiled.."<<std::endl;
     return 0;
 }
