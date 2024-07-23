ab2a3432ebaf87db340f3da5cccdd0714ef73875
JUNZ1 <gigabayt@gmail.com>
2024-07-23 20:02:03 +0200

    Google Test integration is enhanced

--- mainCPP/main.cpp
+++ mainCPP/main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-#include "curl/curl.h"
-using namespace std;
+// Testi yapılacak basit bir fonksiyon
+int Sum(int a, int b) {
+    return a + b;
+}
 
-int main() {
-    std::cout<<"compiled.."<<std::endl;
-    return 0;
+// Test case: Sum fonksiyonunu test eder
+TEST(SumTest, HandlesPositiveInput) {
+    EXPECT_EQ(Sum(1, 2), 3);
+    EXPECT_EQ(Sum(10, 20), 30);
+}
+
+// Test case: Sum fonksiyonunu test eder
+TEST(SumTest, HandlesNegativeInput) {
+    EXPECT_EQ(Sum(-1, -1), -2);
+    EXPECT_EQ(Sum(-1, 1), 0);
+}
+
+// Ana fonksiyon: Google Test'i çalıştırır
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
